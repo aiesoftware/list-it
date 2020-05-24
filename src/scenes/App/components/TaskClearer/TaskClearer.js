51443d3185c480ef6a74f81e43d6e14652dbb8b5
Alex Eves <alex.eves@aiesoftware.com>
2020-05-24 14:04:28 +0100

    clean up - now functional

--- src/scenes/App/components/TaskClearer/TaskClearer.js
+++ src/scenes/App/components/TaskClearer/TaskClearer.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 
-import classes from './TaskClearer.module.css';
 import buttonClasses from '../../../../components/Button/Button.module.css';
 import {clearTasks} from '../../../../services/tasks/actions';
 
